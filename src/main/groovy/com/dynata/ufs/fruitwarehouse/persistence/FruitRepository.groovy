891f9a63d88e845ec63cb880f618ee605e645fcd
David Eduardo Vazquez Vargas <vvdavided@gmail.com>
2022-10-04 17:09:14 -0500

    Complete requirements

--- src/main/groovy/com/dynata/ufs/fruitwarehouse/persistence/FruitRepository.groovy
+++ src/main/groovy/com/dynata/ufs/fruitwarehouse/persistence/FruitRepository.groovy
@@ -2,9 +2,13 @@ package com.dynata.ufs.fruitwarehouse.persistence
 
 import com.dynata.ufs.fruitwarehouse.model.Fruit
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface FruitRepository extends JpaRepository<Fruit, Long> {
+
+    @Query(value = "select fruit_name, color, count(color) from fruit group by fruit_name, color", nativeQuery = true)
+    List findAllCount()
 
 }
--- src/test/groovy/com/dynata/ufs/fruitwarehouse/FruitWarehouseApplicationTests.groovy
+++ src/test/groovy/com/dynata/ufs/fruitwarehouse/FruitWarehouseApplicationTests.groovy
@@ -1,5 +1,6 @@
 package com.dynata.ufs.fruitwarehouse
 
+import com.dynata.ufs.fruitwarehouse.model.Fruit
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -8,6 +9,16 @@ class FruitWarehouseApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+
+	@Test
+	public void testConcatenate() {
+		Fruit fruit = new Fruit();
+
+		boolean result = fruit.setFruitName("onion");
+
+		assert (false == result)
 	}
 
 }
